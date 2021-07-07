24125b3f4052b184dae731dd31c0627c2250973c
Melina Boedecker <melinamboedecker@gmail.com>
2021-07-07 14:02:25 -0700

    change connection to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -3,12 +3,8 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL,
-    {
-      logging: false, 
-      ssl: { rejectUnauthorized: false }
-    });
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
